ff3e666a73d3814e7cc58280ce2c1a8c8430a2f0
Иван Котельников <ivankotelnikov@MacBook-Air-Ivan.local>
2023-02-28 23:52:16 +0300

    add task 10

---
+++ HomeWork-28-02-2023/Task10.cs
@@ -0,0 +1,11 @@
+﻿Console.Clear();
+Console.WriteLine("Введите любое трехзначное число");
+int n = Convert.ToInt32(Console.ReadLine());
+if (n <= 99 || n >= 1000)
+    {
+        Console.WriteLine("Ошибка. Вы ввели " + n + ". Введите трехзначное число.");
+        
+        return;}
+
+int result = n / 10 % 10;
+Console.WriteLine("Вторая цифра трехзначного числа: " + result);
